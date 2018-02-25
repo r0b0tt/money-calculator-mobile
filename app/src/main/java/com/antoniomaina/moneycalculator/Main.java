f679a3165c12f063f6704360906679347480b234
r0b0t <antoniomainakn@gmail.com>
2018-02-25 22:33:49 +0300

    Added options menu

--- app/src/main/java/com/antoniomaina/moneycalculator/Main.java
+++ app/src/main/java/com/antoniomaina/moneycalculator/Main.java
@@ -2,6 +2,9 @@ package com.antoniomaina.moneycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
@@ -22,6 +25,27 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.main);
 
         initialize();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_help:
+                Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void initialize() {
